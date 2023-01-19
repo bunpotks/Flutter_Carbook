a40d36579d9d3d7941f6e6b04196ce567327e31f
Bunpot Klaisorn <103012749+noteyn51@users.noreply.github.com>
2023-01-19 09:49:27 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -231,7 +231,7 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Carbook Officecc',
+      title: 'Flutter Carbook Office',
       theme: theme(),
       locale: _locale,
       localizationsDelegates: [
