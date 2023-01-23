dbf9781a59ea3f7ebb4d3a57c5f8b075c4a8b19e
Bunpot Klaisorn <103012749+noteyn51@users.noreply.github.com>
2023-01-23 16:37:28 +0700

    test update

--- lib/screens/test/test_screen.dart
+++ lib/screens/test/test_screen.dart
@@ -2,7 +2,6 @@ import 'package:carbook/service/alert_dialog.dart';
 import 'package:carbook/service/fetch_service.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 class TestApiModel {
   String? id;
