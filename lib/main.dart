d2097776b7e9128892225c73e7e9d8ce2c96d64b
Bunpot <103012749+noteyn51@users.noreply.github.com>
2023-01-23 18:35:55 +0700

    develop

--- lib/main.dart
+++ lib/main.dart
@@ -231,7 +231,7 @@ class _MyAppState extends State<MyApp> {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Carbook Office',
+      title: 'Flutter Carbook Develop',
       theme: theme(),
       locale: _locale,
       localizationsDelegates: [
