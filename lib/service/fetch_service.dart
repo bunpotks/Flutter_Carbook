e91b9fc7133a9870f4c64cba9cdda01e4f0bd908
Bunpot Klaisorn <103012749+noteyn51@users.noreply.github.com>
2023-01-23 15:37:13 +0700

    setup dialog service

---
+++ lib/service/fetch_service.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class FetchService {
+  String url = "http://localhost:8888";
+  dynamic body;
+
+  FetchService() {
+    // print('123');
+  }
+
+  Future<dynamic> post(data, bodyRequest) async {
+    url += data;
+    body = bodyRequest;
+
+    final response = await http.post(Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(body));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return null;
+    }
+  }
+}
